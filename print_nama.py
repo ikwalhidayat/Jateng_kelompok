fb2b0ee9a13c40c8baeb6fa1c9d3bd660f7acc1c
ikwalhidayat <69829010+ikwalhidayat@users.noreply.github.com>
2024-03-06 10:12:31 +0700

    Create print_nama.py
    nanti temen-temen yang lain silahkan di isi namanya masing masing . jadi masing masing orang coomit namanya di list nama_kelompok

---
+++ print_nama.py
@@ -0,0 +1,12 @@
+def nama_jateng(nama_list):
+    for nama in nama_list:
+        print(nama)
+
+
+nama_kelompok = ["Aditya Hadi Wijaya",
+"L. D. Ikwal Hidayat",
+"Putri Cathliniya Diyanti Faliha"
+"Feriman"]
+
+
+nama_jateng(nama_kelompok)
